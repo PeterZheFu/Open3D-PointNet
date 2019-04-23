ceb38a17ed4e14acae9cde3a42d21b348c791abc
Zhe Fu <fuzhe@yahoo.com>
2019-04-23 16:09:16 -0700

    new logs after debugged xyzs to xyz for master

--- train_classification.py
+++ train_classification.py
@@ -91,7 +91,7 @@ for epoch in range(opt.nepoch):
 
         current_train_result_numpy = np.array([[epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize)]])
         train_result_numpy = np.concatenate((train_result_numpy, current_train_result_numpy), axis = 0)
-        pd.DataFrame(train_result_numpy).to_csv("log_xyzs_train.csv")
+        pd.DataFrame(train_result_numpy).to_csv("log_xyz_train.csv")
 
         if i % 10 == 0:
             j, data = next(enumerate(testdataloader, 0))
@@ -109,6 +109,6 @@ for epoch in range(opt.nepoch):
 
             current_test_result_numpy = np.array([[epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize)]])
             test_result_numpy = np.concatenate((test_result_numpy, current_test_result_numpy), axis = 0)
-            pd.DataFrame(test_result_numpy).to_csv("log_xyzs_test.csv")
+            pd.DataFrame(test_result_numpy).to_csv("log_xyz_test.csv")
 
     torch.save(classifier.state_dict(), '%s/cls_model_xyz_%d.pth' % (opt.outf, epoch))
